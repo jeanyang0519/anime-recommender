fb83f920131414ad045d01e4407fb56589f7089a
jeanyang0519 <sy2803@tc.columbia.edu>
2025-04-07 15:30:48 -0600

    created by Jean

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,14 @@
 export default function Footer() {
   return (
     <footer className="text-right text-sm text-gray-500 px-6 py-4 w-full">
-      Created by{" "}
+      
       <a
         href="https://jean-yang.com"
         target="_blank"
         rel="noopener noreferrer"
         className="text-gray-500 hover:underline"
       >
-        Jean
+       Created by Jean
       </a>
     </footer>
   );
